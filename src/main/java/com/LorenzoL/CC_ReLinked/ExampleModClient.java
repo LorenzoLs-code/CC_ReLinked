093b95b5304840890e0ad5ce72d1a97188789bd0
LorenzoL <221478738+LorenzoLs-code@users.noreply.github.com>
2026-08-23 19:34:17 +0200

    Added a recipe for the cablehub

--- src/main/java/com/LorenzoL/CC_ReLinked/ExampleModClient.java
+++ src/main/java/com/LorenzoL/CC_ReLinked/ExampleModClient.java
@@ -25,7 +25,7 @@ public class ExampleModClient {
     @SubscribeEvent
     static void onClientSetup(FMLClientSetupEvent event) {
         // Some client setup code
-        CC_Relinked.LOGGER.info("HELLO FROM CLIENT SETUP");
-        CC_Relinked.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        /*CC_Relinked.LOGGER.info("HELLO FROM CLIENT SETUP");
+        CC_Relinked.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());*/
     }
 }
