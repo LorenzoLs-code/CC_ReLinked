c73875f739979934d704e8e66ac706f2df962724
LorenzoL <221478738+LorenzoLs-code@users.noreply.github.com>
2026-08-24 20:57:26 +0200

    Fixed the model error for the data glasses and did some other stuff

--- src/main/java/com/LorenzoL/CC_ReLinked/peripheral/DataGlassesPeripheral.java
+++ src/main/java/com/LorenzoL/CC_ReLinked/peripheral/DataGlassesPeripheral.java
@@ -1,8 +1,14 @@
 package com.LorenzoL.CC_ReLinked.peripheral;
 
+import com.LorenzoL.CC_ReLinked.item.ModArmorMaterials;
+import com.LorenzoL.CC_ReLinked.item.ModItems;
 import dan200.computercraft.api.peripheral.GenericPeripheral;
 import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 
 public class DataGlassesPeripheral implements GenericPeripheral {
     @Override
@@ -11,6 +17,22 @@ public class DataGlassesPeripheral implements GenericPeripheral {
     }
 
     public static void RenderOverlay(GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
-        // code
+        // ====== checks =====
+        // check if the player is in the menu or something like that
+        Player player = Minecraft.getInstance().player;
+        if (player == null) { return; }
+
+        // check if the glasses are on
+        ItemStack helmet = player.getItemBySlot(EquipmentSlot.HEAD);
+        if (helmet.getItem() !=  ModItems.DataGlasses.get()) { return; }
+
+        // ====== Actual Code ======
+        guiGraphics.drawString(
+                Minecraft.getInstance().font,
+                "Hello World",
+                10, 10,
+                0xFFFFFF
+        );
+
     }
 }
