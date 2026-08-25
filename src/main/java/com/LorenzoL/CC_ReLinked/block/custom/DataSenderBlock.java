cc78ea02ab210a9d093ee17b3ea08d691fe78707
LorenzoL <221478738+LorenzoLs-code@users.noreply.github.com>
2026-08-25 19:57:25 +0200

    ..

--- src/main/java/com/LorenzoL/CC_ReLinked/block/custom/DataSenderBlock.java
+++ src/main/java/com/LorenzoL/CC_ReLinked/block/custom/DataSenderBlock.java
@@ -1,12 +1,21 @@
 package com.LorenzoL.CC_ReLinked.block.custom;
 
 import com.LorenzoL.CC_ReLinked.block.entity.DataSenderBlockEntity;
+import com.LorenzoL.CC_ReLinked.item.ModItems;
+import com.LorenzoL.CC_ReLinked.peripheral.DataGlassesPeripheral;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import org.jspecify.annotations.Nullable;
 
 public class DataSenderBlock extends BaseEntityBlock {
@@ -28,5 +37,45 @@ public class DataSenderBlock extends BaseEntityBlock {
     @Override
     public @Nullable BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
         return new DataSenderBlockEntity(blockPos, blockState);
+    }
+
+    /* events stuff */
+    @Override
+    protected void onPlace(BlockState state, Level level, BlockPos pos, BlockState oldState, boolean movedByPiston) {
+        super.onPlace(state, level, pos, oldState, movedByPiston);
+        if (level.isClientSide) return;
+        if (state.getBlock() == oldState.getBlock()) return;
+
+        // create block in OverlayData
+        DataGlassesPeripheral.OverlayData.put(
+                DataGlassesPeripheral.getOverlayDataKey(pos),
+                DataGlassesPeripheral.NullRenderData()
+        );}
+    @Override
+    protected void onRemove(BlockState state, Level level, BlockPos pos, BlockState newState, boolean movedByPiston) {
+        if (level.isClientSide) return;
+
+        // remove block form OverlayData
+        DataGlassesPeripheral.OverlayData.remove(
+                DataGlassesPeripheral.getOverlayDataKey(pos)
+        );
+
+        super.onRemove(state, level, pos, newState, movedByPiston);
+    }
+
+    @Override
+    protected ItemInteractionResult useItemOn(
+            ItemStack stack,
+            BlockState state,
+            Level level,
+            BlockPos pos,
+            Player player,
+            InteractionHand hand,
+            BlockHitResult hitResult
+    ) {
+        if (stack.getItem() != ModItems.DataGlasses.get()) { return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION; }
+        
+        // code
+        return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
     }
 }
--- src/main/java/com/LorenzoL/CC_ReLinked/peripheral/DataGlassesPeripheral.java
+++ src/main/java/com/LorenzoL/CC_ReLinked/peripheral/DataGlassesPeripheral.java
@@ -6,9 +6,16 @@ import dan200.computercraft.api.peripheral.GenericPeripheral;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DataGlassesPeripheral implements GenericPeripheral {
     @Override
@@ -16,6 +23,20 @@ public class DataGlassesPeripheral implements GenericPeripheral {
         return "cc_relinked:dataglasses_peripheral";
     }
 
+    // ===== OVERLAY =====
+    // ===== Data Storage & Utils
+    public static Map<List<Integer>, Map<Integer, List<List<Object>>>> OverlayData;
+    public static Map<Integer, List<List<Object>>> NullRenderData() {
+        Map<Integer, List<List<Object>>> M = new HashMap<>();
+        List<List<Object>> N = new ArrayList<>();
+        M.put(0, N);
+
+        return M;}
+    public static List<Integer> getOverlayDataKey(BlockPos pos) {
+        return List.of(pos.getX(), pos.getY(), pos.getZ());
+    }
+
+    // ===== Rendering
     public static void RenderOverlay(GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
         // ====== checks =====
         // check if the player is in the menu or something like that
@@ -27,12 +48,23 @@ public class DataGlassesPeripheral implements GenericPeripheral {
         if (helmet.getItem() !=  ModItems.DataGlasses.get()) { return; }
 
         // ====== Actual Code ======
-        guiGraphics.drawString(
-                Minecraft.getInstance().font,
-                "Hello World",
-                10, 10,
-                0xFFFFFF
-        );
 
+        try {
+            Map<Integer, List<List<Object>>> RenderData = OverlayData.get("");
+            for (List<Object> l : RenderData.get(0)) {
+                guiGraphics.drawString(
+                        Minecraft.getInstance().font,           // font
+                        (String) l.get(0),                      // text (0)
+                        (Integer) l.get(1), (Integer) l.get(2),  // x (1), y (2)
+                        0xFFFFFF);                              // color
+            }
+        } catch (Exception e) {
+            guiGraphics.drawString(
+                    Minecraft.getInstance().font,
+                    "ERROR: False RenderData", 5, 5, 0xFF0000);
+            guiGraphics.drawString(
+                    Minecraft.getInstance().font,
+                    "please report this error to CC: ReLinked", 5, 15, 0xFF0000);
+        }
     }
 }
